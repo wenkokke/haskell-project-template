55f8f6ac15d6e4bef17fe30302c0d56999f56682
Wen Kokke <wenkokke@users.noreply.github.com>
2022-09-28 19:02:36 +0100

    Delete .cache

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -8,10 +8,17 @@ PROJECT_ROOT = os.path.realpath(os.path.curdir)
 
 if __name__ == "__main__":
 
-    # Remove test_directory if 'use_test' is not 'y'
+    # Remove test_directory if 'use_test' is not 'y':
     if "{{ cookiecutter.use_test }}" != "y":
         shutil.rmtree(os.path.join(PROJECT_ROOT, "{{cookiecutter.test_directory}}"))
 
-    # Run pre-commit
-    PRE_COMMIT_CONFIG = os.path.join(PROJECT_ROOT, ".pre-commit-config.yaml")
-    subprocess.check_call(f"pre-commit run --config {PRE_COMMIT_CONFIG} --files ./**/*", cwd=PROJECT_ROOT)
+    # Initialize a git repository with a github remote if 'use_git' is 'github':
+    if "{{ cookiecutter.git_init }}" == "github":
+        github_remote_url = "{{cookiecutter.__github_remote_url}}"
+        subprocess.check_call(f"git init", cwd=PROJECT_ROOT, shell=True)
+        subprocess.check_call(f"git remote add origin {github_remote_url}", cwd=PROJECT_ROOT, shell=True)
+
+    # Run pre-commit if 'use_pre_commit' is 'y':
+    if "{{ cookiecutter.use_pre_commit }}" == "y":
+        subprocess.check_call(f"pre-commit run --files ./**/*", cwd=PROJECT_ROOT, shell=True)
+
