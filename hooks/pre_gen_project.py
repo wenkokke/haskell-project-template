47589ddf256528aa2be06fe3c43c335a682f61a0
Wen Kokke <wenkokke@users.noreply.github.com>
2022-09-28 16:37:49 +0100

    Update defaults

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -17,42 +17,42 @@ if source_directory == test_directory:
 # Check that ghc_version and cabal_version are valid
 ################################################################################
 
-import os
-import sys
+# import os
+# import sys
 
-TEMPLATE_ROOT = os.path.join(os.path.dirname(__file__), os.path.pardir)
+# TEMPLATE_ROOT = os.path.join(os.path.dirname(__file__), os.path.pardir)
 
-sys.path.append(TEMPLATE_ROOT)
+# sys.path.append(TEMPLATE_ROOT)
 
-import local_extensions
+# import local_extensions
 
-versions = local_extensions.HaskellVersions()
+# versions = local_extensions.HaskellVersions()
 
-ghc_version = "{{cookiecutter.ghc_version}}"
+# ghc_version = "{{cookiecutter.ghc_version}}"
 
-cabal_version = "{{cookiecutter.cabal_version}}"
+# cabal_version = "{{cookiecutter.cabal_version}}"
 
-# Verify that ghc_version is a SemVer version number:
-if ghc_version != "latest" and not versions.is_semver(ghc_version):
-    print(f"ERROR: invalid GHC version '{ghc_version}'")
-    exit(1)
+# # Verify that ghc_version is a SemVer version number:
+# if ghc_version != "latest" and not versions.is_semver(ghc_version):
+#     print(f"ERROR: invalid GHC version '{ghc_version}'")
+#     exit(1)
 
-# Verify that cabal_version is a SemVer version number:
-if cabal_version != "latest" and not versions.is_semver(cabal_version):
-    print(f"ERROR: invalid GHC version '{cabal_version}'")
-    exit(1)
+# # Verify that cabal_version is a SemVer version number:
+# if cabal_version != "latest" and not versions.is_semver(cabal_version):
+#     print(f"ERROR: invalid GHC version '{cabal_version}'")
+#     exit(1)
 
 
-# Verify ghc_version against the list of known GHC versions:
-if ghc_version != "latest" and versions.ghc_versions and ghc_version not in versions.ghc_versions:
-    print(
-        f"ERROR: unknown GHC version '{ghc_version}', not in {versions.ghc_versions}'"
-    )
-    exit(1)
+# # Verify ghc_version against the list of known GHC versions:
+# if ghc_version != "latest" and versions.ghc_versions and ghc_version not in versions.ghc_versions:
+#     print(
+#         f"ERROR: unknown GHC version '{ghc_version}', not in {versions.ghc_versions}'"
+#     )
+#     exit(1)
 
-# Verify cabal_version against the list of known cabal versions:
-if cabal_version != "latest" and versions.cabal_versions and cabal_version not in versions.cabal_versions:
-    print(
-        f"ERROR: unknown cabal version '{cabal_version}', not in {versions.cabal_versions}'"
-    )
-    exit(1)
+# # Verify cabal_version against the list of known cabal versions:
+# if cabal_version != "latest" and versions.cabal_versions and cabal_version not in versions.cabal_versions:
+#     print(
+#         f"ERROR: unknown cabal version '{cabal_version}', not in {versions.cabal_versions}'"
+#     )
+#     exit(1)
