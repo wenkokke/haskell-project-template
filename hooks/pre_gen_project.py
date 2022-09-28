38d4a7ceb4de8ce0fb8a5c616fbf06812d252d95
Wen Kokke <wenkokke@users.noreply.github.com>
2022-09-28 16:35:37 +0100

    Verify GHC and cabal versions

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -17,51 +17,42 @@ if source_directory == test_directory:
 # Check that ghc_version and cabal_version are valid
 ################################################################################
 
-import re
-import requests
+import os
+import sys
 
-VERSIONS_JSON_URL = (
-    "https://raw.githubusercontent.com/haskell/actions/main/setup/src/versions.json"
-)
+TEMPLATE_ROOT = os.path.join(os.path.dirname(__file__), os.path.pardir)
 
-REGEX_SEMVER = re.compile(
-    r"^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?"
-)
+sys.path.append(TEMPLATE_ROOT)
+
+import local_extensions
+
+versions = local_extensions.HaskellVersions()
 
 ghc_version = "{{cookiecutter.ghc_version}}"
 
 cabal_version = "{{cookiecutter.cabal_version}}"
 
 # Verify that ghc_version is a SemVer version number:
-if ghc_version != "latest" and not REGEX_SEMVER.match(ghc_version):
+if ghc_version != "latest" and not versions.is_semver(ghc_version):
     print(f"ERROR: invalid GHC version '{ghc_version}'")
     exit(1)
 
 # Verify that cabal_version is a SemVer version number:
-if cabal_version != "latest" and not REGEX_SEMVER.match(cabal_version):
+if cabal_version != "latest" and not versions.is_semver(cabal_version):
     print(f"ERROR: invalid GHC version '{cabal_version}'")
     exit(1)
 
-# If there are concrete versions, verify them against the list of known versions:
-if ghc_version != "latest" or cabal_version != "latest":
-    try:
-        # Get versions.json from the setup-haskell action:
-        response = requests.get(VERSIONS_JSON_URL)
-        response.raise_for_status()
-        versions = response.json()
-        # Verify ghc_version against the list of known GHC versions:
-        known_ghc_versions = list(versions["ghc"])
-        if ghc_version not in known_ghc_versions:
-            print(
-                f"ERROR: unknown GHC version '{ghc_version}', not in {known_ghc_versions}'"
-            )
-            exit(1)
-        # Verify cabal_version against the list of known GHC versions:
-        known_cabal_versions = list(versions["cabal"])
-        if cabal_version not in known_cabal_versions:
-            print(
-                f"ERROR: unknown cabal version '{cabal_version}', not in {known_cabal_versions}'"
-            )
-            exit(1)
-    except (requests.exceptions.HTTPError, requests.exceptions.JSONDecodeError):
-        print(f"WARNING: could not retrieve known GHC and cabal versions")
+
+# Verify ghc_version against the list of known GHC versions:
+if ghc_version != "latest" and versions.ghc_versions and ghc_version not in versions.ghc_versions:
+    print(
+        f"ERROR: unknown GHC version '{ghc_version}', not in {versions.ghc_versions}'"
+    )
+    exit(1)
+
+# Verify cabal_version against the list of known cabal versions:
+if cabal_version != "latest" and versions.cabal_versions and cabal_version not in versions.cabal_versions:
+    print(
+        f"ERROR: unknown cabal version '{cabal_version}', not in {versions.cabal_versions}'"
+    )
+    exit(1)
--- local_extensions/__init__.py
+++ local_extensions/__init__.py
@@ -1,21 +1,125 @@
-from jinja2.ext import Extension
+import collections
+import json
+import logging
+import os
 import re
+import requests
+
+LOGGER = logging.getLogger(__name__)
 
 
-class HaskellExtension(Extension):
+class HaskellVersions:
+    REGEX_SEMVER = re.compile(
+        r"^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?"
+    )
+
+    def is_semver(self, version: str) -> bool:
+        return bool(self.__class__.REGEX_SEMVER.match(version))
+
+    VERSIONS_JSON_URL = (
+        "https://raw.githubusercontent.com/haskell/actions/main/setup/src/versions.json"
+    )
+
+    VERSIONS_JSON_FILE = os.path.join(
+        os.path.dirname(__file__), os.path.pardir, "versions.json"
+    )
+
+    def resolve_latest_cabal_version(self, cabal_version: str) -> str:
+        if cabal_version == "latest":
+            return self.latest_cabal_version
+        else:
+            return cabal_version
+
+    @property
+    def latest_cabal_version(self):
+        if self.cabal_versions:
+            return self.cabal_versions[0]
+        else:
+            return "latest"
+
+    @property
+    def cabal_versions(self):
+        return self.versions["cabal"]
+
+    def resolve_latest_ghc_version(self, ghc_version: str) -> str:
+        if ghc_version == "latest":
+            return self.latest_ghc_version
+        else:
+            return ghc_version
+
+    @property
+    def latest_ghc_version(self):
+        if self.ghc_versions:
+            return self.ghc_versions[0]
+        else:
+            return "latest"
+
+    @property
+    def ghc_versions(self):
+        return self.versions["ghc"]
+
+    @property
+    def versions(self):
+        if self._versions is None:
+            if os.path.exists(self.__class__.VERSIONS_JSON_FILE):
+                self._versions = self._get_versions_from_file()
+            else:
+                self._versions = self._get_versions_from_url()
+                with open(self.__class__.VERSIONS_JSON_FILE, "wb") as fp:
+                    json.dump(self._versions, fp)
+        return self._versions
+
+    def _get_versions_from_file(self):
+        if os.path.exists(self.__class__.VERSIONS_JSON_FILE):
+            try:
+                with open(self.__class__.VERSIONS_JSON_FILE, "rb") as fp:
+                    return json.load(fp)
+            except (json.JSONDecodeError,) as e:
+                LOGGER.warning(f"Could not load versions from file: {e}")
+        return collections.defaultdict(default_factory=list)
+
+    def _get_versions_from_url(self):
+        try:
+            response = requests.get(self.__class__.VERSIONS_JSON_URL)
+            response.raise_for_status()
+            return response.json()
+        except (
+            requests.exceptions.HTTPError,
+            requests.exceptions.JSONDecodeError,
+        ) as e:
+            LOGGER.warning(f"Could not load versions from URL: {e}")
+            return collections.defaultdict(default_factory=list)
+
+
+class HaskellStrCase:
+
     REGEX_DELIMITER = re.compile(r"[-_.:/](?!\s)+")
 
-    def __init__(self, environment):
-        super(HaskellExtension, self).__init__(environment)
-        environment.filters["to_pascal"] = HaskellExtension.to_pascal
-
-    @staticmethod
     def to_pascal(
+        self,
         text: str,
     ) -> str:
         return "".join(
             [
                 chunk.capitalize()
-                for chunk in HaskellExtension.REGEX_DELIMITER.sub(" ", text).split()
+                for chunk in self.REGEX_DELIMITER.sub(" ", text).split()
             ]
         )
+
+
+try:
+    import jinja2.ext
+
+    class HaskellExtension(jinja2.ext.Extension, HaskellVersions, HaskellStrCase):
+        def __init__(self, environment):
+            super(HaskellExtension, self).__init__(environment)
+            environment.filters["to_pascal"] = self.to_pascal
+            environment.filters[
+                "resolve_latest_cabal_version"
+            ] = self.resolve_latest_cabal_version
+            environment.filters[
+                "resolve_latest_ghc_version"
+            ] = self.resolve_latest_ghc_version
+
+except ModuleNotFoundError as e:
+    pass
