1853c24837673deebd1eb55cbbb6a8d1943711b9
Wen Kokke <wenkokke@users.noreply.github.com>
2022-09-28 21:02:18 +0100

    Catch non-zero output for pre-commit first run; use more variables in post-gen

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,27 +1,45 @@
 #!/usr/bin/env python
+import logging
 import os
 import shutil
 import subprocess
+
+LOGGER = logging.getLogger(__name__)
 
 PROJECT_ROOT = os.path.realpath(os.path.curdir)
 
 
 if __name__ == "__main__":
 
+    use_test = "{{cookiecutter.use_test}}"
+    use_git_init = "{{cookiecutter.use_git_init}}"
+    use_pre_commit = "{{cookiecutter.use_pre_commit}}"
+
     # Remove test_directory if 'use_test' is not 'y':
-    if "{{cookiecutter.use_test}}" != "y":
+    if use_test != "y":
         shutil.rmtree(os.path.join(PROJECT_ROOT, "{{cookiecutter.test_directory}}"))
 
-    # Initialize a git repository with a github remote if 'use_git' is 'github':
-    if "{{cookiecutter.use_git_init}}" == "github":
-        github_remote_url = "{{cookiecutter.__github_remote_url}}"
+    # Initialize a git repository if 'use_git_init' is not 'n':
+    if use_git_init != "n":
         subprocess.check_call(f"git init", cwd=PROJECT_ROOT, shell=True)
-        subprocess.check_call(
-            f"git remote add origin {github_remote_url}", cwd=PROJECT_ROOT, shell=True
-        )
+        # ... with a github remote if 'use_git_init' is 'github':
+        if use_git_init == "github":
+            github_remote_url = "{{cookiecutter.__github_remote_url}}"
+            subprocess.check_call(
+                f"git remote add origin {github_remote_url}",
+                cwd=PROJECT_ROOT,
+                shell=True,
+            )
 
     # Run pre-commit if 'use_pre_commit' is 'y':
-    if "{{cookiecutter.use_pre_commit}}" == "y":
-        subprocess.check_call(
-            f"pre-commit run --files ./**/*", cwd=PROJECT_ROOT, shell=True
-        )
+    if use_pre_commit == "y":
+        try:
+            subprocess.check_call(
+                f"pre-commit run --files ./**/*", cwd=PROJECT_ROOT, shell=True
+            )
+        except subprocess.CalledProcessError as e:
+            LOGGER.warning(e)
+
+    # Install pre-commit if 'use_pre_commit' is 'y' and 'use_git_init' is not 'n'
+    if use_pre_commit == "y" and use_git_init != "n":
+        subprocess.check_call(f"pre-commit install", cwd=PROJECT_ROOT, shell=True)
