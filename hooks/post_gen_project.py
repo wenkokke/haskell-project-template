99fbe34f515d3f69d173c2a5d229c2131c1aad9f
Wen Kokke <wenkokke@users.noreply.github.com>
2022-09-28 20:40:16 +0100

    Minor change

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -9,11 +9,11 @@ PROJECT_ROOT = os.path.realpath(os.path.curdir)
 if __name__ == "__main__":
 
     # Remove test_directory if 'use_test' is not 'y':
-    if "{{ cookiecutter.use_test }}" != "y":
+    if "{{cookiecutter.use_test}}" != "y":
         shutil.rmtree(os.path.join(PROJECT_ROOT, "{{cookiecutter.test_directory}}"))
 
     # Initialize a git repository with a github remote if 'use_git' is 'github':
-    if "{{ cookiecutter.use_git_init }}" == "github":
+    if "{{cookiecutter.use_git_init}}" == "github":
         github_remote_url = "{{cookiecutter.__github_remote_url}}"
         subprocess.check_call(f"git init", cwd=PROJECT_ROOT, shell=True)
         subprocess.check_call(
@@ -21,7 +21,7 @@ if __name__ == "__main__":
         )
 
     # Run pre-commit if 'use_pre_commit' is 'y':
-    if "{{ cookiecutter.use_pre_commit }}" == "y":
+    if "{{cookiecutter.use_pre_commit}}" == "y":
         subprocess.check_call(
             f"pre-commit run --files ./**/*", cwd=PROJECT_ROOT, shell=True
         )
