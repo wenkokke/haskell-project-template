3c6d6c33913404dd7489bf43df4332cba5525e45
Wen Kokke <wenkokke@users.noreply.github.com>
2022-09-28 18:05:51 +0100

    Use git config

--- local_extensions/__init__.py
+++ local_extensions/__init__.py
@@ -1,3 +1,4 @@
+from .git_config import GitConfigExtension as GitConfigExtension
 from .haskell_versions import HaskellVersionsExtension as HaskellVersionsExtension
 from .strcase import StrCaseExtension as StrCaseExtension
 from .version import VersionExtension as VersionExtension
---
+++ local_extensions/git_config.py
@@ -0,0 +1,26 @@
+import jinja2.ext  # type: ignore
+import subprocess
+import typing
+
+
+class GitConfigExtension(jinja2.ext.Extension):
+    def __init__(self, environment):
+        super().__init__(environment)
+        environment.filters["resolve_fullname"] = self.resolve_fullname
+        environment.filters["resolve_email"] = self.resolve_email
+
+    def resolve_fullname(self, fullname: typing.Optional[str]):
+        if fullname is None:
+            try:
+                return subprocess.getoutput("git config --get user.name")
+            except subprocess.CalledProcessError as e:
+                raise ValueError(f"Could not resolve fullname, {e}")
+        return fullname
+
+    def resolve_email(self, email: typing.Optional[str]):
+        if email is None:
+            try:
+                return subprocess.getoutput("git config --get user.email")
+            except subprocess.CalledProcessError as e:
+                raise ValueError(f"Could not resolve email, {e}")
+        return email
