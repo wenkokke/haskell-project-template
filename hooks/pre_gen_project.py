60cd725286b74cb1abb834f23278cdb82c0833e7
Wen Kokke <wenkokke@users.noreply.github.com>
2022-09-28 14:48:41 +0100

    Add cabal_version

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python
+import re
+
+################################################################################
+# Check that source_directory and test_directory are distinct
+################################################################################
 
 source_directory = "{{cookiecutter.source_directory}}"
 test_directory = "{{cookiecutter.test_directory}}"
@@ -8,3 +13,19 @@ if source_directory == test_directory:
         f"ERROR: 'source_directory' and 'test_directory' cannot be the same directory: {source_directory}"
     )
     exit(1)
+
+################################################################################
+# Check that ghc_version and cabal_version are valid
+################################################################################
+
+ghc_version = "{{cookiecutter.ghc_version}}"
+
+if ghc_version != "latest" and not re.match(r"\d+\.\d+\.\d+", ghc_version):
+    print(f"ERROR: invalid GHC version '{ghc_version}'")
+    exit(2)
+
+cabal_version = "{{cookiecutter.cabal_version}}"
+
+if cabal_version != "latest" and not re.match(r"(\d+\.\d+)(\.\d+)?", cabal_version):
+    print(f"ERROR: invalid GHC version '{cabal_version}'")
+    exit(2)
